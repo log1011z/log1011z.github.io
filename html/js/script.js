f46313efa7b72941b8609f7d221c91d515221a38
log1011z <2830614475@qq.com>
2023-02-26 22:38:37 +0800

    Site updated: 2023-02-26 22:38:36

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,4 @@
-dycalendar.draw({
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>dycalendar.draw({
   target: '#dycalendar',
   type: 'month',
   highlighttargetdate: true,
